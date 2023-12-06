fcb3e2ec16b63165f9aa06d89554c19adbcbd7b3
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2023-12-06 16:55:50 -0300

    ajusta o estilo do tabuleiro

--- tabuleiro.js
+++ tabuleiro.js
@@ -11,7 +11,7 @@ function criarCasas(tabuleiro) {
 
     for (let i = 0; i < 3; i++) {
       const placar = document.createElement('span');
-      placar.calssname = 'placar';
+      placar.className = 'placar';
       placar.id = `placar-${i}`;
 
       tabuleiro.appendChild(placar);
