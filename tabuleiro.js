b96b3c3263a1be9caf0cf5ad65a5682df2f517dd
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2023-12-06 16:49:28 -0300

    adiciona o placar embaixo de cada coluna

--- tabuleiro.js
+++ tabuleiro.js
@@ -3,9 +3,19 @@
 function criarCasas(tabuleiro) {
     for (let i = 0; i < 9; i++) {
       const casa = document.createElement('div');
-      casa.className = 'celula';
+      casa.className = 'casa';
+      casa.id = `casa-${i}`;
 
       tabuleiro.appendChild(casa);
+    }
+
+    for (let i = 0; i < 3; i++) {
+      const placar = document.createElement('span');
+      placar.calssname = 'placar';
+      placar.id = `placar-${i}`;
+
+      tabuleiro.appendChild(placar);
+      placar.innerText = 0;
     }
 }
 
