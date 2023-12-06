eba50d2e6b90f09860251c3a74d4ecb9d9c8b386
Guilherme Coutinho <guilhermecoutinho579@gmail.com>
2023-12-06 17:36:44 -0300

    cria listas para atualizar casas

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ const tabuleiroJogador = document.getElementById('tabuleiro-jogador');
 
 criarCasas(tabuleiroOponente, 0);
 criarCasas(tabuleiroJogador, 1);
+
+
+const colunaUmjogador = [0, 0, 0];
+const colunaDoisjogador = [0, 0, 0];
+const colunaTresjogador = [0, 0, 0];
+
+const colunaUmOponente = [0, 0, 0];
+const colunaDoisOponente = [0, 0, 0];
+const colunatresOponente = [0, 0, 0];
